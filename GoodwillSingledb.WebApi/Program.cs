3cf3c21438e3e6e9f8e354126ecc5a5d742ee01c
Pavel <68848355+Palchak-Pavel@users.noreply.github.com>
2024-09-27 15:38:05 +0300

    Не запускается приложение

--- GoodwillSingledb.WebApi/Program.cs
+++ GoodwillSingledb.WebApi/Program.cs
@@ -3,11 +3,18 @@ using GoodwillSingledb.Application.Common.Mappings;
 using GoodwillSingledb.Application.Interfaces;
 using Microsoft.Extensions.Configuration;
 using GoodwillSingledb.Application;
+using System.Reflection;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
 var host = WebApplication.CreateBuilder(args).Build();
 var devCorsPolicy = "devCorsPolicy";
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IGoodwillSingleDbContext).Assembly));
+});
 //builder.Services.AddCors(options =>
 //{
 //    options.AddPolicy(devCorsPolicy, builder => {
@@ -15,12 +22,19 @@ var devCorsPolicy = "devCorsPolicy";
 //    });
 //});
 
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(GoodwillSingledb.Application.DependencyInjection).Assembly);
+});
+//builder.Services.AddMediatR(typeof(GoodwillSingledb.Application.DependencyInjection).Assembly);
+//builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 builder.Services.AddScoped<IGoodwillSingleDbContext, GoodwillSingleDbContext>();
 builder.Services.AddAutoMapper(typeof(AssemblyMappingProfile));
 builder.Services.AddApplication();
 //builder.Services.AddPersistence(Configuration);
 builder.Services.AddControllers();
-
+var app = builder.Build();
 host.Run();
 //}
 app.UseHttpsRedirection();
